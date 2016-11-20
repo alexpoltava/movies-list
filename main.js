8e90e17d71e182e7f3f0a39cd1e3c263f4a64e8e
alexpoltava <a.n.omelchenko@icloud.com>
2016-11-20 23:57:38 +0200

    need to develop table sort method

--- main.js
+++ main.js
@@ -1,4 +1,31 @@
 
+var crteateTable = function (list){
+console.log(list);
+var table = $("<table></table>").appendTo("#container");
+var headerRow = $("<tr></tr>").appendTo(table);
+$("<th></th>").text("ID").appendTo(headerRow);
+$("<th></th>").text("Adult").appendTo(headerRow);
+$("<th></th>").text("Original Title").appendTo(headerRow);
+$("<th></th>").text("Language").appendTo(headerRow);
+$("<th></th>").text("Popularity Index").appendTo(headerRow);
+$("<th></th>").text("Votes Count").appendTo(headerRow);
+$("<th></th>").text("Rating").appendTo(headerRow);
+$("<th></th>").text("Release Date").appendTo(headerRow);
+
+  for (i=0; i<list.length; i++)
+  {
+    var row = $("<tr></tr>").appendTo(table);
+    $("<td></td>").text(list[i].id).appendTo(row);
+    $("<td></td>").text(list[i].adult).appendTo(row);
+    $("<td></td>").text(list[i].original_title).appendTo(row);
+    $("<td></td>").text(list[i].original_language).appendTo(row);
+    $("<td></td>").text(list[i].popularity).appendTo(row);
+    $("<td></td>").text(list[i].vote_count).appendTo(row);
+    $("<td></td>").text(list[i].vote_average).appendTo(row);
+    $("<td></td>").text(list[i].release_date).appendTo(row);
+  }
+}
+
 function findMovies(Name)
 {
   if (Name.length>0)
@@ -21,11 +48,10 @@ function findMovies(Name)
           return new Error("Unexpected error");
       }
     })
-    .then(function(list)
+    .then(function(response)
     {
-      console.log(list);
+      crteateTable(response.results);
     });
-
   }else{
       return new Error("Empty search string");
   }
